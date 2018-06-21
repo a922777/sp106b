bcc1c825c820e7235077ffc8231ea022b537ea5a
s110510548 <31913858+a922777@users.noreply.github.com>
2018-06-21 14:10:44 +0800

    Update Assembler.java

--- exercise/06/Assembler.java
+++ exercise/06/Assembler.java
@@ -16,7 +16,7 @@ public class Assembler {
     public static HashMap<String,String> jmpMap = new HashMap<String, String>();
 
     static {
-        //put all predefined vars into a HashMap
+        //symTable
         cMap.put("SP",0);cMap.put("LCL",1);cMap.put("ARG",2);cMap.put("THIS",3);
         cMap.put("THAT",4);cMap.put("R0",0);cMap.put("R1",1);cMap.put("R2",2);
         cMap.put("R3",3);cMap.put("R4",4);cMap.put("R5",5);cMap.put("R6",6);
@@ -24,26 +24,23 @@ public class Assembler {
         cMap.put("R11",11);cMap.put("R12",12);cMap.put("R13",13);cMap.put("R14",14);
         cMap.put("R15",15);cMap.put("SCREEN",16384);cMap.put("KBD",24576);
 
-        //for c instructions comp=dst;jmp
-        //put all comp posibilities with A into a HashMap,a=0
+        //ctable
         compAMap.put("0","101010");compAMap.put("1","111111");compAMap.put("-1","111010");
         compAMap.put("D","001100");compAMap.put("A","110000");compAMap.put("!D","001101");
         compAMap.put("!A","110001");compAMap.put("-D","001111");compAMap.put("-A","110011");
         compAMap.put("D+1","011111");compAMap.put("A+1","110111");compAMap.put("D-1","001110");
         compAMap.put("A-1","110010");compAMap.put("D+A","000010");compAMap.put("D-A","010011");
         compAMap.put("A-D","000111");compAMap.put("D&A","000000");compAMap.put("D|A","010101");
-
-        //put all comp posibilities with M into a HashMap,a=1
         compMMap.put("M","110000");compMMap.put("!M","110001");compMMap.put("-M","110011");
         compMMap.put("M+1","110111");compMMap.put("M-1","110010");compMMap.put("D+M","000010");
         compMMap.put("D-M","010011");compMMap.put("M-D","000111");compMMap.put("D&M","000000");
         compMMap.put("D|M","010101");
 
-        //put all dst posibilities into a HashMap
+        //dtable
         dstMap.put("","000");dstMap.put("M","001");dstMap.put("D","010");dstMap.put("MD","011");
         dstMap.put("A","100");dstMap.put("AM","101");dstMap.put("AD","110");dstMap.put("AMD","111");
 
-        //put all jmp posibilities into a HashMap
+        //jtable
         jmpMap.put("","000");jmpMap.put("JGT","001");jmpMap.put("JEQ","010");jmpMap.put("JGE","011");
         jmpMap.put("JLT","100");jmpMap.put("JNE","101");jmpMap.put("JLE","110");jmpMap.put("JMP","111");
     }
